ac59d612ba64db840c3443b87edc4a0bb23ccebf
Sgm <servm4@yandex.ru>
2023-05-19 11:57:39 +0300

    add homework

---
+++ 03 Классы и объекты/01 Калькулятор двух чисел/Калькулятор двух чисел.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+
+class Calculator {
+private:
+    double num1, num2;
+
+public:
+    Calculator(double n1, double n2) {
+        num1 = n1;
+        num2 = n2;
+    }
+
+    double add() {
+        return num1 + num2;
+    }
+
+    double multiply() {
+        return num1 * num2;
+    }
+
+    double subtract_1_2() {
+        return num1 - num2;
+    }
+    double subtract_2_1() {
+        return num2 - num1;
+    }
+    double divide_1_2() {
+        return num1 / num2;
+    }
+    double divide_2_1() {
+        return num2 / num1;
+    }
+    bool set_num1(double num1) {
+        if (num1 != 0) {
+            this->num1 = num1;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    bool set_num2(double num2) {
+        if (num2 != 0) {
+            this->num2 = num2;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
+
+int main() {
+    double number1, number2;
+
+    while (1 != 2) {
+        std::cout << "Enter number 1: ";
+        std::cin >> number1;
+
+        std::cout << "Enter number 2: ";
+        std::cin >> number2;
+
+        Calculator calc(number1, number2);
+
+        std::cout << "num1 + num2 = " << calc.add() << std::endl;
+        std::cout << "num1 * num2 = " << calc.multiply() << std::endl;
+        std::cout << "num1 - num2 = " << calc.subtract_1_2() << std::endl;
+        std::cout << "num2 - num1 = " << calc.subtract_2_1() << std::endl;
+        std::cout << "num1 / num2 = " << calc.divide_1_2() << std::endl;
+        std::cout << "num2 / num1 = " << calc.divide_2_1() << std::endl;
+    }
+
+    return 0;
+}
---
+++ 03 Классы и объекты/02 Счетчик/Счетчик.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <string>
+#include "windows.h"
+
+class Counter {
+private:
+    int num;
+
+public:
+    Counter(int n) {
+        num = n;
+    }
+    int add() {
+        num += 1;
+        return num;
+    }
+    int subtract() {
+        num -= 1;
+        return num;
+    }
+    int getValue() {
+        return num;
+    }
+};
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int number = 1;
+    char sym;
+    std::string answer;
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    getline(std::cin, answer);
+    if (answer == "да") {
+        std::cout << "Введите начальное значение счётчика: ";
+        std::cin >> number;
+    }
+    Counter count(number);
+    while (1 != 2) {
+        std::cout << "Введите команду ('+', '-', '=' или 'x'):\n";
+        std::cin >> sym;
+
+
+        if (sym == '+') {
+            count.add();
+        }
+        else if (sym == '-') {
+            count.subtract();
+        }
+        else if (sym == '=') {
+            std::cout << count.getValue() << std::endl;
+        }
+        else if (sym == 'x') {
+            std::cout << "До свидания!";
+            break;
+        }
+
+
+    }
+
+    return 0;
+}
