5ee7bced85e8fc1ef622aa70b4dc421bbfa45c87
Sgm <servm4@yandex.ru>
2023-05-24 15:56:00 +0300

    version 2

--- 04 ООП абстракция и инкапсуляция/Задача 2. Сортируем адреса/Сортируем адреса.cpp
+++ 04 ООП абстракция и инкапсуляция/Задача 2. Сортируем адреса/Сортируем адреса.cpp
@@ -3,43 +3,38 @@
 #include <fstream>
 #include "windows.h"
 
-class adresses
-{
+
+class adresses {
+private:
+    std::string city;
+    std::string street;
+    int house;
+    int apartment;
+
 public:
-    std::string city, street;
-    int house, apartment;
-};
+    adresses(const std::string& city, const std::string& street, int house, int apartment)
+        : city(city), street(street), house(house), apartment(apartment) {}
 
-
-
-int main() {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    std::ifstream fin("in.txt");
-    std::string temp;
-    int temp2, N, less;
-    int count = 0;
-    fin >> N;
-    int* sort = new int[N];
-    adresses* objects = new adresses[N];
-
-    for (int i = 0; i < N; ++i) {
-        fin >> temp;
-        objects[i].city = temp;
-        fin >> temp;
-        objects[i].street = temp;
-        fin >> temp2;
-        objects[i].house = temp2;
-        fin >> temp2;
-        objects[i].apartment = temp2;
+    std::string getCity() const {
+        return city;
     }
 
-    fin.close();
+    std::string getStreet() const {
+        return street;
+    }
 
-    std::ofstream fout("out.txt");
+    int getHouse() const {
+        return house;
+    }
 
-    fout << N << std::endl;
-    
+    int getApartment() const {
+        return apartment;
+    }
+};
+
+void sort(adresses* objects, int N) {
+    int less;
+
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
             if (objects[i].city.size() < objects[j].city.size()) {
@@ -61,16 +56,46 @@ int main() {
                 }
                 count = 0;
             }
-            
+
         }
     }
+}
 
-        for (int i = 0; i < N; ++i) {
-            fout << objects[i].city << ", " << objects[i].street << ", " << objects[i].house << ", " << objects[i].apartment << std::endl;
-        }
-        
-        fout.close();
-    
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::ifstream fin("in.txt");
+    std::string temp;
+    int temp2, N;
+    int count = 0;
+    fin >> N;
+    adresses* objects = new adresses[N];
+
+    for (int i = 0; i < N; ++i) {
+        fin >> temp;
+        objects[i].city = temp;
+        fin >> temp;
+        objects[i].street = temp;
+        fin >> temp2;
+        objects[i].house = temp2;
+        fin >> temp2;
+        objects[i].apartment = temp2;
+    }
+
+    fin.close();
+
+    std::ofstream fout("out.txt");
+
+    sort(objects, N);
+
+    fout << N << std::endl;
+
+    for (int i = 0; i < N; ++i) {
+        fout << objects[i].city << ", " << objects[i].street << ", " << objects[i].house << ", " << objects[i].apartment << std::endl;
+    }
+
+    fout.close();
+
 
     delete[] objects;
     return 0;
