3c72838d88f348343bf522db4e2525b71649402a
Sgm <servm4@yandex.ru>
2023-05-25 15:57:42 +0300

    add homework exercise 1

---
+++ 05 ООП наследование и полиморфизм/Задача 1 Фигуры. Количество сторон/Фигуры. Количество сторон.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <string>
+
+
+class Figure {
+protected:
+	int sides_count;
+	std::string name;
+public:
+
+	Figure(int num, std::string n) : sides_count(num), name(n) {}
+
+	int get_sides_count() {
+		return sides_count;
+	}
+	std::string what() {
+		return name;
+	}
+	void display() {
+		std::cout << what() << get_sides_count() << std::endl;
+	}
+};
+
+
+class Triangle : public Figure {
+public:
+	Triangle(int sides_count, std::string name) : Figure(sides_count, name) {}
+
+	void setNumber(int num) {
+		sides_count = num;
+	}
+	void setName(std::string n) {
+		name = n;
+	}
+
+	void display() {
+		std::cout << what() << get_sides_count() << std::endl;
+	}
+};
+
+
+class Quadrangle : public Figure {
+public:
+	Quadrangle(int sides_count, std::string name) : Figure(sides_count, name) {}
+
+	void setNumber(int num) {
+		sides_count = num;
+	}
+	void setName(std::string n) {
+		name = n;
+	}
+
+	void display() {
+		std::cout << what() << get_sides_count() << std::endl;
+	}
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	std::cout << "Количество сторон: \n";
+	Figure figure(0, "Фигура: ");
+	figure.display();
+	Triangle triangle(3, "Треугольник: ");
+	triangle.display();
+	Quadrangle quadrangle(4, "Квадрат: ");
+	quadrangle.display();
+
+
+
+
+}
+
