e0614f0e5fd8286398974a7417144d331b98d839
Sgm <servm4@yandex.ru>
2023-06-08 17:50:39 +0300

    Version 3

--- 08 Исключения и обработка ошибок/02 Исключения в конструкторах/MySource.cpp
+++ 08 Исключения и обработка ошибок/02 Исключения в конструкторах/MySource.cpp
@@ -13,64 +13,150 @@
 #include "Rhombus.h"
 
 
-
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	Check checker;
-
-
+	int sides, a, b, c, d, e, f, g, h;
 	try {
-		if (checker.checkTriangleSides(3) == false) {
-			throw "Что - то не то со сторонами";
+		sides = 3;
+		if (sides != 3) {
+			throw "Ошибка создания фигуры. Причина: количество сторон";
 		}
-		if (checker.checkTriangleCorners(50, 60, 90) == false) {
-			throw "Что - то не то с углами";
+		Triangle triangle(3, "Треугольник:\n");
+		d = 50;
+		e = 60;
+		f = 70;
+		if (d + e + f != 180) {
+			throw "Ошибка создания фигуры. Причина: сумма углов не равна 180";
 		}
-		Triangle triangle(3, "Треугольник: \n");
-		triangle.set_lenght_and_corners(10, 20, 30, 50, 60, 90);
+		triangle.set_lenght_and_corners(10, 20, 30, d, e, f);
 		triangle.print_info();
+
+		rightTriangle rightTriangle(3, "Прямоугольный треугольник:\n");
+		f = 90;
+		if (f != 90) {
+			throw "Ошибка создания фигуры. Причина: угол C не равен 90";
+		}
+		rightTriangle.set_lenght_and_corners(10, 20, 30, 50, 60, f);
+		rightTriangle.print_info();
+		a = 10;
+		c = 10;
+		if (a != c) {
+			throw "Ошибка создания фигуры. Причина: сторона A не равна стороне C";
+		}
+		isoscelesTriangle isoscelesTriangle(3, "Равнобедренный треугольник: \n");
+		d = 50;
+		f = 50;
+		if (d != f) {
+			throw "Ошибка создания фигуры. Причина: угол А не равен углу C";
+		}
+		isoscelesTriangle.set_lenght_and_corners(a, 20, c, d, 60, f);
+		isoscelesTriangle.print_info();
+		a = 30;
+		b = 30;
+		c = 30;
+		if (a != b && a != c) {
+			throw "Ошибка создания фигуры. Причина: стороны не равны";
+		}
+		equilateraltriangle equilateraltriangle(3, "Равносторонний треугольник: \n");
+		d = 60;
+		e = 60;
+		f = 60;
+		if (d != e && d != f) {
+			throw "Ошибка создания фигуры. Причина: углы не равны";
+		}
+		equilateraltriangle.set_lenght_and_corners(a, b, c, d, e, f);
+		equilateraltriangle.print_info();
+
+		sides = 4;
+		if (sides != 4) {
+			throw "Ошибка создания фигуры. Причина: количество сторон";
+		}
+		Quadrangle quadrangle(4, "Четырехугольник: \n");
+		e = 50;
+		f = 60;
+		g = 70;
+		h = 80;
+		if (50 + 60 + 70 + 80 != 360) {
+			throw "Ошибка создания фигуры. Причина: сумма углов не равна 360 градусов";
+		}
+		quadrangle.set_lenght_and_corners(10, 20, 30, 40, e, f, g, h);
+		quadrangle.print_info();
+
+		a = 10;
+		b = 20;
+		c = 10;
+		d = 20;
+		if (a != c && b != d) {
+			throw "Ошибка создания фигуры. Причина: стороны попарно не равны";
+		}
+		Rectangle rectangle(4, "Прямоугольник: \n");
+		e = 50;
+		f = 60;
+		g = 70;
+		h = 80;
+		if (e != 90 && f != 90 && g != 90 && h != 90) {
+			throw "Ошибка создания фигуры. Причина: углы не равны 90";
+		}
+		rectangle.set_lenght_and_corners(a, b, c, d, e, f, g, h);
+		rectangle.print_info();
+
+		a = 20;
+		b = 20;
+		c = 20;
+		d = 20;
+		if (a != b && a != c && a != d) {
+			throw "Ошибка создания фигуры. Причина: стороны не равны";
+		}
+		Square square(4, "Квадрат: \n");
+		e = 90;
+		f = 90;
+		g = 90;
+		h = 90;
+		if (e != 90 && f != 90 && g != 90 && h != 90) {
+			throw "Ошибка создания фигуры. Причина: углы не равны 90";
+		}
+		square.set_lenght_and_corners(a, b, c, d, e, f, g, h);
+		square.print_info();
+
+		a = 20;
+		b = 20;
+		c = 20;
+		d = 20;
+		if (a != b && a != c && a != d) {
+			throw "Ошибка создания фигуры. Причина: стороны попарно не равны";
+		}
+		Parallelogram parallelogram(4, "Параллелограм: \n");
+		e = 90;
+		f = 90;
+		g = 90;
+		h = 90;
+		if (e != g && f != h) {
+			throw "Ошибка создания фигуры. Причина: углы попарно не равны";
+		}
+		parallelogram.set_lenght_and_corners(a, b, c, d, 30, 40, 30, 40);
+		parallelogram.print_info();
+
+		a = 30;
+		b = 30;
+		c = 30;
+		d = 30;
+		if (a != b && a != c && a != d) {
+			throw "Ошибка создания фигуры. Причина: стороны не равны";
+		}
+
+		Rhombus rhombus(4, "Ромб: \n");
+		e = 30;
+		f = 40;
+		g = 30;
+		h = 40;
+		if (30 != 30 && 40 != 40) {
+			throw "Ошибка создания фигуры. Причина: углы попарно не равны";
+		}
+		rhombus.set_lenght_and_corners(a, b, c, d, e, f, g, h);
+		rhombus.print_info();
 	}
 	catch (const char* exception) {
 		std::cout << exception << std::endl;
 	}
-	
-
-	
-	rightTriangle rightTriangle(3, "Прямоугольный треугольник:\n");
-	rightTriangle.set_lenght_and_corners(10, 20, 30, 50, 60, 90);
-	rightTriangle.print_info();
-
-	isoscelesTriangle isoscelesTriangle(3, "Равнобедренный треугольник: \n");
-	isoscelesTriangle.set_lenght_and_corners(10, 20, 10, 50, 60, 50);
-	isoscelesTriangle.print_info();
-
-	equilateraltriangle equilateraltriangle(3, "Равносторонний треугольник: \n");
-	equilateraltriangle.set_lenght_and_corners(30, 30, 30, 60, 60, 60);
-	equilateraltriangle.print_info();
-
-	Quadrangle quadrangle(4, "Четырехугольник: \n");
-	quadrangle.set_lenght_and_corners(10, 20, 30, 40, 50, 60, 70, 80);
-	quadrangle.print_info();
-
-	Rectangle rectangle(4, "Прямоугольник: \n");
-	rectangle.set_lenght_and_corners(10, 20, 10, 20, 90, 90, 90, 90);
-	rectangle.print_info();
-
-	Square square(4, "Квадрат: \n");
-	square.set_lenght_and_corners(20, 20, 20, 20, 90, 90, 90, 90);
-	square.print_info();
-
-	Parallelogram parallelogram(4, "Параллелограм: \n");
-	parallelogram.set_lenght_and_corners(20, 30, 20, 30, 30, 40, 30, 40);
-	parallelogram.print_info();
-
-	Rhombus rhombus(4, "Ромб: \n");
-	rhombus.set_lenght_and_corners(30, 30, 30, 30, 30, 40, 30, 40);
-	rhombus.print_info();
-
-
-
-
 }
-
