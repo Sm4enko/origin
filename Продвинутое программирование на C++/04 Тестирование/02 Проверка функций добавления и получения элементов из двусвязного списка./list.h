d4d7637ab6f231e21289718af5e625b130dcde14
Sm4enko <131249115+Sm4enko@users.noreply.github.com>
2023-08-25 08:53:59 +0300

    Version 2

--- Продвинутое программирование на C++/04 Тестирование/02 Проверка функций добавления и получения элементов из двусвязного списка./list.h
+++ Продвинутое программирование на C++/04 Тестирование/02 Проверка функций добавления и получения элементов из двусвязного списка./list.h
@@ -1,7 +1,6 @@
 #ifndef LIST_H
 #define LIST_H
 
-#include <iostream>
 #include <stdexcept>
 
 struct ListNode
@@ -20,76 +19,23 @@ public:
     ListNode* next;
 };
 
-
 class List
 {
 public:
-    List()
-        : m_head(new ListNode(static_cast<int>(0))), m_size(0),
-        m_tail(new ListNode(0, m_head))
-    {
-    }
+    List();
+    virtual ~List();
 
-    virtual ~List()
-    {
-        Clear();
-        delete m_head;
-        delete m_tail;
-    }
+    bool Empty();
+    unsigned long Size();
 
-    bool Empty() { return m_size == 0; }
-
-    unsigned long Size() { return m_size; }
-
-    void PushFront(int value)
-    {
-        new ListNode(value, m_head, m_head->next);
-        ++m_size;
-    }
-
-    void PushBack(int value)
-    {
-        new ListNode(value, m_tail->prev, m_tail);
-        ++m_size;
-    }
-
-    int PopFront()
-    {
-        if (Empty()) throw std::runtime_error("list is empty");
-        auto node = extractPrev(m_head->next->next);
-        int ret = node->value;
-        delete node;
-        return ret;
-    }
-
-    int PopBack()
-    {
-        if (Empty()) throw std::runtime_error("list is empty");
-        auto node = extractPrev(m_tail);
-        int ret = node->value;
-        delete node;
-        return ret;
-    }
-
-    void Clear()
-    {
-        auto current = m_head->next;
-        while (current != m_tail)
-        {
-            current = current->next;
-            delete extractPrev(current);
-        }
-    }
+    void PushFront(int value);
+    void PushBack(int value);
+    int PopFront();
+    int PopBack();
+    void Clear();
 
 private:
-    ListNode* extractPrev(ListNode* node)
-    {
-        auto target = node->prev;
-        target->prev->next = target->next;
-        target->next->prev = target->prev;
-        --m_size;
-        return target;
-    }
+    ListNode* extractPrev(ListNode* node);
 
 private:
     ListNode* m_head;
@@ -97,5 +43,5 @@ private:
     unsigned long m_size;
 };
 
-#endif // LIST_H
+#endif
 
--- Продвинутое программирование на C++/04 Тестирование/02 Проверка функций добавления и получения элементов из двусвязного списка./main.cpp
+++ Продвинутое программирование на C++/04 Тестирование/02 Проверка функций добавления и получения элементов из двусвязного списка./main.cpp
@@ -1,23 +1,63 @@
-#include <iostream>
-#include <stdexcept>
-#include "List.h"
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include "list.h"
 
-int main() {
-    List myList;
+TEST_CASE("PushBack and PushFront") {
+    List list;
+    list.PushBack(1);
+    list.PushFront(2);
+    list.PushBack(3);
 
-    myList.PushBack(1);
-    myList.PushFront(2);
-    myList.PushBack(3);
-    myList.PushFront(4);
+    REQUIRE(list.Size() == 3);
 
-    std::cout << "List size: " << myList.Size() << std::endl;
+    SECTION("Check values after PushBack and PushFront") {
+       
+    }
+}
 
-    while (!myList.Empty()) {
-        std::cout << "PopBack: " << myList.PopBack() << std::endl;
+TEST_CASE("PopBack and PopFront") {
+    List list;
+    REQUIRE_THROWS_AS(list.PopBack(), std::runtime_error);
+    REQUIRE_THROWS_AS(list.PopFront(), std::runtime_error);
+
+    SECTION("PopBack and PopFront from a non-empty list") {
+        list.PushBack(1);
+        list.PushFront(2);
+
+        REQUIRE(list.PopBack() == 1);
+        REQUIRE(list.PopFront() == 2);
+
+        REQUIRE(list.Empty());
     }
 
-    std::cout << "List size after popping all elements: " << myList.Size() << std::endl;
-
-    return 0;
+    SECTION("PopBack and PopFront from an empty list") {
+        REQUIRE_THROWS_AS(list.PopBack(), std::runtime_error);
+        REQUIRE_THROWS_AS(list.PopFront(), std::runtime_error);
+    }
 }
+
+TEST_CASE("Pop from an empty list") {
+    List list;
+    REQUIRE_THROWS_AS(list.PopBack(), std::runtime_error);
+    REQUIRE_THROWS_AS(list.PopFront(), std::runtime_error);
+}
+
+TEST_CASE("Pop and Push on non-empty list") {
+    List list;
+    list.PushBack(1);
+    list.PushFront(2);
+
+    REQUIRE(list.PopBack() == 1);
+    REQUIRE(list.PopFront() == 2);
+    REQUIRE(list.Empty());
+
+    list.PushBack(3);
+    list.PushFront(4);
+
+    REQUIRE(list.PopBack() == 3);
+    REQUIRE(list.PopFront() == 4);
+    REQUIRE(list.Empty());
+}
+
+
 
