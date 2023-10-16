9fa28b4b69efae391c542bcf1d1fbb97d2be662a
Sm4enko <servm4@yandex.ru>
2023-10-16 11:54:27 +0300

    new

--- Работа с базами данных/04 Работа с PostgreSQL из C++/ConsoleApplication1/ConsoleApplication1.cpp
+++ Работа с базами данных/04 Работа с PostgreSQL из C++/ConsoleApplication1/ConsoleApplication1.cpp
@@ -18,74 +18,62 @@ public:
             "last_name VARCHAR(50), "
             "phone_number VARCHAR(20), "
             "address TEXT[])";
-        exec(sql);
+        pqxx::work txn(*connection);
+        txn.exec(sql);
+        txn.commit();
     }
 
     void insertData(const std::string& firstName, const std::string& lastName, const std::string& phoneNumber, const std::vector<std::string>& email) {
-        std::string sql = "INSERT INTO people (first_name, last_name, phone_number, address) VALUES ($1, $2, $3, $4)";
-        exec(sql, firstName, lastName, phoneNumber, email);
+        pqxx::work txn(*connection);
+        pqxx::result result = txn.prepared("stmt", "INSERT INTO people (first_name, last_name, phone_number, address) VALUES ($1, $2, $3, $4)")(
+            firstName)(lastName)(phoneNumber)(email);
+        txn.commit();
     }
 
     void updateData(int id, const std::string& firstName, const std::string& lastName, const std::string& phoneNumber, const std::vector<std::string>& email) {
-        std::string sql = "UPDATE people SET first_name = $2, last_name = $3, phone_number = $4, address = $5 WHERE id = $1";
-        exec(sql, id, firstName, lastName, phoneNumber, email);
+        pqxx::work txn(*connection);
+        pqxx::result result = txn.prepared("stmt", "UPDATE people SET first_name = $2, last_name = $3, phone_number = $4, address = $5 WHERE id = $1")(
+            id)(firstName)(lastName)(phoneNumber)(email);
+        txn.commit();
     }
 
     void deleteData(int id) {
-        std::string sql = "DELETE FROM people WHERE id = $1";
-        exec(sql, id);
+        pqxx::work txn(*connection);
+        pqxx::result result = txn.prepared("stmt", "DELETE FROM people WHERE id = $1")(id);
+        txn.commit();
     }
 
     pqxx::result queryData(const std::string& selectQuery) {
-        return query(selectQuery);
+        pqxx::work txn(*connection);
+        return txn.exec(selectQuery);
     }
 
 private:
     pqxx::connection* connection;
-
-    exec("INSERT INTO people (first_name, last_name, phone_number, email) VALUES ($1, $2, $3, $4)", firstName, lastName, phoneNumber, email);
-
-    pqxx::result result = query("SELECT * FROM people WHERE first_name = $1 AND last_name = $2", firstName, lastName);
-
+};
 
 int main() {
     TransactionBase db;
 
     try {
-        pqxx::work txn(*db.connection);
+        db.createTable();
+        std::vector<std::string> email = { "123 Main St", "456 Elm St" };
+        db.insertData("John", "Doe", "+phoneNumber", email);
+        db.updateData(1, "Jane", "Doe", "+phoneNumber", email);
 
-        // Create a prepared statement for later use
-        txn.prepared("stmt", "INSERT INTO people (first_name, last_name, phone_number, address) VALUES ($1, $2, $3, $4)");
-        txn.prepared("stmt", "UPDATE people SET first_name = $2, last_name = $3, phone_number = $4, address = $5 WHERE id = $1");
-        txn.prepared("stmt", "DELETE FROM people WHERE id = $1");
-
-        txn.commit();
+        // Query data
+        pqxx::result result = db.queryData("SELECT * FROM people");
+        for (const pqxx::tuple& row : result) {
+            std::cout << "ID: " << row["id"].as<int>() << ", First Name: " << row["first_name"].as<std::string>() <<
+                ", Last Name: " << row["last_name"].as<std::string>() << ", Phone Number: " << row["phone_number"].as<std::string>() <<
+                ", Address: " << row["address"].as<std::string>() << std::endl;
+        }
+        db.deleteData(1);
     }
     catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
         return 1;
     }
-
-    // Create the 'people' table
-    db.createTable();
-
-    // Insert some data
-    std::vector<std::string> email = { "123 Main St", "456 Elm St" };
-    db.insertData("John", "Doe", phoneNumber, "sldfjl@dsjfl.dsjalfj");
-
-    // Update data
-    db.updateData(1, "Jane", "Doe", "+phoneNumber", "jsdlfjl@gjalsdjfgls.coaf");
-
-    // Query data
-    pqxx::result result = db.queryData("SELECT * FROM people");
-    for (const pqxx::tuple& row : result) {
-        std::cout << "ID: " << row["id"].as<int>() << ", First Name: " << row["first_name"].as<std::string>() <<
-            ", Last Name: " << row["last_name"].as<std::string>() << ", Phone Number: " << row["phone_number"].as<std::string>() <<
-            ", Address: " << row["address"].as<std::string>() << std::endl;
-    }
-
-    // Delete data
-    db.deleteData(1);
 
     return 0;
 }
