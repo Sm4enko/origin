13b9f49b90908acd693b535cc0effd305e97bf9f
Sm4enko <servm4@yandex.ru>
2023-11-14 13:31:36 +0300

    add homework

---
+++ Многопоточное и асинхронное программирование/03 Асинхронное программирование/01 Сортировка выбором/ConsoleApplication4.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <future>
+#include <algorithm>
+
+void sorting(std::vector<int>& vec, std::promise<std::vector<int>> promise) {
+
+    for (int i = 0; i < vec.size() - 1; ++i) {
+        int minIndex = i;
+        for (int j = i + 1; j < vec.size(); ++j) {
+            if (vec[j] < vec[minIndex]) {
+                minIndex = j;
+            }
+        }
+        std::swap(vec[i], vec[minIndex]);
+    }
+
+    promise.set_value(vec);
+}
+
+int main() {
+ 
+    std::vector<int> numbers = { 2, 15, 1, 33, 42 };
+
+    std::promise<std::vector<int>> promise;
+    std::future<std::vector<int>> future = promise.get_future();
+
+    std::thread sortingThread(sorting, std::ref(numbers), std::move(promise));
+
+    std::vector<int> toSort = future.get();
+
+    std::cout << "Sorted numbers: ";
+    for (int num : toSort) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    sortingThread.join();
+
+    return 0;
+}
---
+++ Многопоточное и асинхронное программирование/03 Асинхронное программирование/02 For_each/For_each.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <future>
+
+template <typename Iterator, typename Function>
+void parallel(Iterator begin, Iterator end, Function func, size_t range) {
+    const size_t size = std::distance(begin, end);
+
+    if (size <= range) {
+        std::for_each(begin, end, func);
+    } else {
+      
+        Iterator middle = begin + size / 2;
+
+        auto future_first_half = std::async(std::launch::async, parallel<Iterator, Function>, begin, middle, func, range);
+
+        parallel(middle, end, func, range);
+
+        future_first_half.wait();
+    }
+}
+
+int main() {
+    std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    
+    auto square = [](int &num) { num *= num; };
+
+    size_t range = 2;
+
+    parallel(numbers.begin(), numbers.end(), square, range);
+
+    for (const auto &num : numbers) {
+        std::cout << num << " ";
+    }
+
+    return 0;
+}
