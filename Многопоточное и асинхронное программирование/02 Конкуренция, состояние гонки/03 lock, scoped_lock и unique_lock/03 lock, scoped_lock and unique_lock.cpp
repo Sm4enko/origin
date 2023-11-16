41e99e8cb9fe8a17077f737320ef01c56aacde5c
Sm4enko <servm4@yandex.ru>
2023-11-16 10:28:55 +0300

    version 3

--- Многопоточное и асинхронное программирование/02 Конкуренция, состояние гонки/03 lock, scoped_lock и unique_lock/03 lock, scoped_lock and unique_lock.cpp
+++ Многопоточное и асинхронное программирование/02 Конкуренция, состояние гонки/03 lock, scoped_lock и unique_lock/03 lock, scoped_lock and unique_lock.cpp
@@ -3,14 +3,15 @@
 #include <mutex>
 #include <algorithm>
 
-class myMutex {
+template <typename T>
+class Data {
 private:
     std::mutex mtx1;
     std::mutex mtx2;
-    int a = 30, b = 50;
+    T a, b;
 
 public:
-    myMutex() : a(0), b(0) {}
+    Data() : a(30), b(50) {}
 
     void Lock() {
         mtx1.lock();
@@ -22,48 +23,49 @@ public:
         mtx2.unlock();
     }
 
-    int GetData1() {
+    T GetData1() {
         return a;
     }
 
-    int GetData2() {
+    T GetData2() {
         return b;
     }
 
-    void SetData1(int value) {
+    void SetData1(T value) {
         a = value;
     }
 
-    void SetData2(int value2) {
+    void SetData2(T value2) {
         b = value2;
     }
 
-    void swap(int value1, int value2) {
+    void swap(T value1, T value2) {
         Lock();
         SetData1(value2);
         SetData2(value1);
         Unlock();
     }
 
-    void swap2(int value1, int value2) {
-        std::unique_lock<std::mutex> lock(mtx1);
+    void swap2(T value1, T value2) {
+        std::lock(mtx1, mtx2);
+        std::lock_guard<std::mutex> lock1(mtx1, std::adopt_lock);
+        std::lock_guard<std::mutex> lock2(mtx2, std::adopt_lock);
         SetData1(value2);
         SetData2(value1);
     }
 
-    void swap3(int value1, int value2) {
+    void swap3(T value1, T value2) {
         std::lock(mtx1, mtx2);
         std::unique_lock<std::mutex> lock1(mtx1, std::adopt_lock);
         std::unique_lock<std::mutex> lock2(mtx2, std::adopt_lock);
         SetData1(value2);
         SetData2(value1);
     }
-
 };
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    myMutex myMutexObject;
+    Data<int> myMutexObject;
 
     std::thread t1([&myMutexObject]() {
         int value1 = 42, value2 = 10;
