2147280ee6c2df8da0898121c82f37f0596e24d0
Sm4enko <servm4@yandex.ru>
2023-11-30 11:20:39 +0300

    add homework

---
+++ Шаблоны проектирования в C++/01 Порождающие шаблоны/01 Little/Growing templates.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <type_traits>
+
+class SqlSelectQueryBuilder {
+public:
+    SqlSelectQueryBuilder() : selectColumns({ "*" }), fromTable("") {}
+
+    SqlSelectQueryBuilder& AddColumn(const std::string& column) {
+        selectColumns.push_back(column);
+        return *this;
+    }
+
+    SqlSelectQueryBuilder& AddFrom(const std::string& table) {
+        fromTable = table;
+        return *this;
+    }
+
+    SqlSelectQueryBuilder& AddWhere(const std::string& col, const std::string& value) {
+        whereClauses.push_back(col + "=" + value);
+        return *this;
+    }
+
+    std::string Create() const {
+        std::string query = "SELECT " + addColumns() + " FROM " + fromTable + addWhereClauses();
+        std::cout << query << std::endl;
+        return query;
+    }
+
+private:
+    std::vector<std::string> selectColumns;
+    std::string fromTable;
+    std::vector<std::string> whereClauses;
+
+    std::string addColumns() const {
+        return (selectColumns.size() > 1) ? JoinStrings(selectColumns, ", ") : selectColumns[0];
+    }
+
+    std::string addWhereClauses() const {
+        return (whereClauses.empty()) ? "" : " WHERE " + JoinStrings(whereClauses, " AND ");
+    }
+
+    std::string JoinStrings(const std::vector<std::string>& strings, const std::string& delimiter) const {
+        std::string result;
+        for (size_t i = 0; i < strings.size(); ++i) {
+            result += strings[i];
+            if (i < strings.size() - 1) {
+                result += delimiter;
+            }
+        }
+        return result;
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    SqlSelectQueryBuilder query_builder;
+    query_builder.AddColumn("name").AddColumn("phone");
+    query_builder.AddFrom("students");
+    query_builder.AddWhere("id", "42").AddWhere("name", "John");
+
+    static_assert(std::is_same<decltype(query_builder.Create()), std::string>::value,
+        "Не совпадает тип запроса");
+
+    return 0;
+}
---
+++ Шаблоны проектирования в C++/01 Порождающие шаблоны/02 More/Growing templates 2.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <type_traits>
+
+class SqlSelectQueryBuilder {
+public:
+    SqlSelectQueryBuilder() : selectColumns({ "*" }), fromTable("") {}
+
+    SqlSelectQueryBuilder& AddColumn(const std::string& cols) {
+        selectColumns.push_back(cols);
+        return *this;
+    }
+
+    SqlSelectQueryBuilder& AddColumns(const std::vector<std::string>& cols) noexcept {
+        selectColumns.insert(selectColumns.end(), cols.begin(), cols.end());
+        return *this;
+    }
+
+    SqlSelectQueryBuilder& AddFrom(const std::string& table) {
+        fromTable = table;
+        return *this;
+    }
+
+    SqlSelectQueryBuilder& AddWhere(const std::string& column, const std::string& value) {
+        whereClauses.push_back(column + "=" + value);
+        return *this;
+    }
+
+    SqlSelectQueryBuilder& AddWhere(const std::map<std::string, std::string>& qq) noexcept {
+        for (const auto& pair : qq) {
+            whereClauses.push_back(pair.first + "=" + pair.second);
+        }
+        return *this;
+    }
+
+    std::string Create() const {
+        std::string query = "SELECT " + addColumns() + " FROM " + fromTable + addWhereClauses();
+        std::cout << query << std::endl; 
+        return query;
+    }
+
+private:
+    std::vector<std::string> selectColumns;
+    std::string fromTable;
+    std::vector<std::string> whereClauses;
+
+    std::string addColumns() const {
+        return (selectColumns.size() > 1) ? JoinStrings(selectColumns, ", ") : selectColumns[0];
+    }
+
+    std::string addWhereClauses() const {
+        return (whereClauses.empty()) ? "" : " WHERE " + JoinStrings(whereClauses, " AND ");
+    }
+
+    std::string JoinStrings(const std::vector<std::string>& strings, const std::string& delimiter) const {
+        std::string result;
+        for (size_t i = 0; i < strings.size(); ++i) {
+            result += strings[i];
+            if (i < strings.size() - 1) {
+                result += delimiter;
+            }
+        }
+        return result;
+    }
+};
+
+int main() {
+    SqlSelectQueryBuilder query_builder;
+    query_builder.AddColumns({ "name", "phone" });
+    query_builder.AddFrom("students");
+    query_builder.AddWhere("id", "42").AddWhere("name", "John");
+
+    query_builder.AddWhere({ {"age", "25"}, {"city", "New York"} });
+
+    auto result = query_builder.Create();
+    static_assert(std::is_same_v<decltype(result), std::string>,
+        "Не совпадает тип запроса");
+
+    return 0;
+}
