772ae2e31ef7a2d85f084bb1f77a882de7d6a5bb
Sergey <servm4@yandex.ru>
2024-05-22 14:43:20 +0300

    Add homework

---
+++ UE5/04 Character Components/Source/LeaveMeAlone/Private/Player/LMADefaultCharacter.cpp
@@ -0,0 +1,95 @@
+// LeaveMeAlone Game by Netologiya. All RightsReserved
+
+
+#include "Player/LMADefaultCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Engine/World.h" 
+#include "Kismet/GameplayStatics.h" 
+#include "Components/DecalComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Engine/DecalActor.h" 
+#include "Components/InputComponent.h"
+
+
+
+ALMADefaultCharacter::ALMADefaultCharacter()
+{
+
+	PrimaryActorTick.bCanEverTick = true;
+
+        SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	SpringArmComponent->SetupAttachment(GetRootComponent());
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
+	CameraComponent->SetupAttachment(SpringArmComponent);
+
+	SpringArmComponent->SetUsingAbsoluteRotation(true);
+	SpringArmComponent->TargetArmLength = ArmLength;
+	SpringArmComponent->SetRelativeRotation(FRotator(YRotation, 0.0f, 0.0f)); 
+	SpringArmComponent->bDoCollisionTest = false;
+	SpringArmComponent->bEnableCameraLag = true;
+	CameraComponent->SetFieldOfView(FOV);
+	CameraComponent->bUsePawnControlRotation = false; 
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+	PlayerStamina = 15;
+
+}
+
+
+void ALMADefaultCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (CursorMaterial)
+	{
+		CurrentCursor = UGameplayStatics::SpawnDecalAtLocation(GetWorld(), CursorMaterial, CursorSize,
+			FVector(0));
+	}
+	
+}
+
+void ALMADefaultCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	APlayerController* PC = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	if (PC)
+	{
+		FHitResult ResultHit;
+		PC->GetHitResultUnderCursor(ECC_GameTraceChannel1, true, ResultHit);
+		float FindRotatorResultYaw = UKismetMathLibrary::FindLookAtRotation(GetActorLocation(),
+			ResultHit.Location).Yaw;
+		SetActorRotation(FQuat(FRotator(0.0f, FindRotatorResultYaw, 0.0f)));
+		if (CurrentCursor)
+		{
+			CurrentCursor->SetWorldLocation(ResultHit.Location);
+		}
+	}
+
+
+}
+
+
+void ALMADefaultCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis("MoveForward", this, &ALMADefaultCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ALMADefaultCharacter::MoveRight);
+
+}
+
+void ALMADefaultCharacter::MoveForward(float Value)
+{
+	AddMovementInput(GetActorForwardVector(), Value);
+}
+void ALMADefaultCharacter::MoveRight(float Value)
+{
+	AddMovementInput(GetActorRightVector(), Value);
+}
+
+
+
+
+
